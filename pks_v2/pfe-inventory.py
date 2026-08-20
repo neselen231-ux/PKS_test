b862d9a01c95d9448ca2db3c383c256ce6e2d013
neselen231-ux <neselen231@gmail.com>
2026-08-20 08:57:30 +0200

    Update pfe-inventory.py

--- pks_v2/pfe-inventory.py
+++ pks_v2/pfe-inventory.py
@@ -198,7 +198,7 @@ if submit:
 
                 #combined.paste(lot_img, (165, 28))
                 #combined.paste(ref_img, (165, ref_img.height+18))
-                combined.paste(dm_img, (290, qty_img.height))
+                combined.paste(dm_img, (290, 200))
                 #combined.paste(vendor_img, (165, vendor_img.height+140))
 
 

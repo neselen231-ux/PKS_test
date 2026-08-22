39b91a5e89fc860e63e98c9abe23e8ea7b6ad0ad
neselen231-ux <neselen231@gmail.com>
2026-08-22 15:44:51 +0200

    Update pfe_reception.py

--- pks_v2/pfe_reception.py
+++ pks_v2/pfe_reception.py
@@ -38,7 +38,7 @@ engine = create_engine(
     }
 )
 
-st.image("pks_test/pks_v2/logo-38023-scaled.jpg",width=280)
+st.image("pks_v2/logo-38023-scaled.jpg",width=280)
 # 2 input boxes
 
 with st.form("input_form"):
